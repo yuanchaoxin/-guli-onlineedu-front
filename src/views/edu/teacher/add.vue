7411f26247689b0a33ad6e2ea3be72f4f22bc923
yuanchaoxin <lboy_2015@163.com>
2022-02-26 13:15:57 +0800

    头像上传

--- src/views/edu/teacher/add.vue
+++ src/views/edu/teacher/add.vue
@@ -22,6 +22,33 @@
       </el-form-item>
 
       <!-- 讲师头像：TODO -->
+      <!-- 讲师头像 -->
+      <el-form-item label="讲师头像">
+
+        <!-- 头衔缩略图 -->
+        <pan-thumb :image="teacher.avatar"/>
+        <!-- 文件上传按钮 -->
+        <el-button type="primary" icon="el-icon-upload" @click="imagecropperShow=true">更换头像
+        </el-button>
+
+        <!--
+      v-show：是否显示上传组件
+      :key：类似于id，如果一个页面多个图片上传控件，可以做区分
+      :url：后台上传的url地址
+      @close：关闭上传组件
+      @crop-upload-success：上传成功后的回调
+        <input type="file" name="file"/>
+      -->
+        <image-cropper
+          v-show="imagecropperShow"
+          :width="300"
+          :height="300"
+          :key="imagecropperKey"
+          :url="BASE_API+'/ossservice/oss/upload'"
+          field="file"
+          @close="close"
+          @crop-upload-success="cropSuccess"/>
+      </el-form-item>
 
       <el-form-item>
         <el-button :disabled="saveBtnDisabled" type="primary" @click="saveOrUpdate">保存</el-button>
@@ -33,11 +60,18 @@
 
 <script>
 import teacherApi from '@/api/edu/teacher'
+import ImageCropper from '@/components/ImageCropper'
+import PanThumb from '@/components/PanThumb'
+
 export default {
+  components: { ImageCropper, PanThumb },
   data() {
     return {
       teacher: {},
-      saveBtnDisabled: false
+      saveBtnDisabled: false,
+      imagecropperShow: false,
+      imagecropperKey: 0,
+      BASE_API: process.env.BASE_API
     }
   },
   watch: {
@@ -49,6 +83,15 @@ export default {
     this.init()
   },
   methods: {
+    close() {
+      this.imagecropperShow = false
+      this.imagecropperKey = this.imagecropperKey + 1
+    },
+    cropSuccess(data) {
+      this.imagecropperShow = false
+      this.teacher.avatar = data.url
+      this.imagecropperKey = this.imagecropperKey + 1
+    },
     init() {
       if (this.$route.params && this.$route.params.id) {
         this.getTeacherInfo(this.$route.params.id)
