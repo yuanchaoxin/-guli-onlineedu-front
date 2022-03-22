47c178e26713aef57352b350b263060f5e81cc6d
yuanchaoxin <lboy_2015@163.com>
2022-03-23 01:21:42 +0800

    权限管理

--- src/api/login.js
+++ src/api/login.js
@@ -1,9 +1,37 @@
 import request from '@/utils/request'
 
+// export function login(username, password) {
+//   return request({
+//     url: '/eduservice/user/login',
+//     // url: '/user/login',
+//     method: 'post',
+//     data: {
+//       username,
+//       password
+//     }
+//   })
+// }
+
+// export function getInfo(token) {
+//   return request({
+//     // url: '/user/info',
+//     url: '/eduservice/user/info',
+//     method: 'get',
+//     params: { token }
+//   })
+// }
+
+// export function logout() {
+//   return request({
+//     url: '/user/logout',
+//     method: 'post'
+//   })
+// }
+// 登录
 export function login(username, password) {
+  // debugger
   return request({
-    url: '/eduservice/user/login',
-    // url: '/user/login',
+    url: '/admin/acl/login',
     method: 'post',
     data: {
       username,
@@ -12,18 +40,28 @@ export function login(username, password) {
   })
 }
 
+// 获取用户信息
 export function getInfo(token) {
   return request({
-    // url: '/user/info',
-    url: '/eduservice/user/info',
+    url: '/admin/acl/index/info',
     method: 'get',
     params: { token }
   })
 }
 
+// 登出
 export function logout() {
+  // debugger
   return request({
-    url: '/user/logout',
+    url: '/admin/acl/index/logout',
     method: 'post'
+  })
+}
+
+// 获取菜单权限数据
+export function getMenu() {
+  return request({
+    url: '/admin/acl/index/menu',
+    method: 'get'
   })
 }
